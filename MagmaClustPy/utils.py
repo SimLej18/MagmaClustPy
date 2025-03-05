8ab599211f6bdcbe4aaa3936175fb814b8a46767
SimLej18 <simonlejoly@icloud.com>
2025-03-05 19:46:01 +0100

    [fix] bug with generated dummy datasets dimensions [enhanc] benchmarking of kernels (in)efficiency on padded inputs

--- MagmaClustPy/utils.py
+++ MagmaClustPy/utils.py
@@ -1,5 +1,22 @@
 import jax.numpy as jnp
+from jax import random
 import pandas as pd
+
+
+def generate_dummy_db(M: int, MIN_N: int, MAX_N: int, grid: jnp.array, key: jnp.array):
+	# We fill DB with random sequences
+	data = []
+	for m in range(M):
+		key, subkey = random.split(key)
+		n_points = random.randint(subkey, (), MIN_N, MAX_N)
+		for n in range(n_points):
+			key, subkey1, subkey2 = random.split(key, 3)
+			data.append({
+				"ID": m,
+				"Input": random.choice(subkey1, grid, (1,))[0].item(),
+				"Output": random.uniform(subkey2, (), jnp.float64, -5, 5).item()
+			})
+	return pd.DataFrame(data)
 
 
 def preprocess_db(db: pd.DataFrame):
@@ -14,7 +31,7 @@ def preprocess_db(db: pd.DataFrame):
 	   - masks: a matrix of shape (M, P) with 1 where the input is valid and 0 where it is padded
 	"""
 	# Get all distinct inputs
-	all_ids = jnp.array(db["ID"])
+	all_ids = jnp.array(db["ID"].unique())
 	all_inputs = jnp.sort(jnp.array(db["Input"].unique()))
 
 	# Initialise padded inputs, padded outputs and masks
