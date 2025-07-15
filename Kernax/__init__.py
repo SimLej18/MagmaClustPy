409125412a613101169b31259478b2b938a2c34f
Grenouil <98542975+Grenouil@users.noreply.github.com>
2025-07-15 11:51:18 +0200

    [new] coded and tested periodic and rational quadratic kernels. Updated the kernels.ipynb notebook by correcting bugs on composed kernels.

--- Kernax/__init__.py
+++ Kernax/__init__.py
@@ -3,6 +3,8 @@ from .RBFKernel import StaticRBFKernel, RBFKernel
 from .LinearKernel import StaticLinearKernel, LinearKernel
 # from .MaternKernel import StaticMaternKernel, MaternKernel
 from .SEMagmaKernel import StaticSEMagmaKernel, SEMagmaKernel
+from .PeriodicKernel import StaticPeriodicKernel, PeriodicKernel
+from .RationalQuadraticKernel import StaticRationalQuadraticKernel, RationalQuadraticKernel
 from .ConstantKernel import StaticConstantKernel, ConstantKernel
 from .OperatorKernels import OperatorKernel, SumKernel, ProductKernel
 from .WrapperKernels import WrapperKernel, NegKernel, ExpKernel, LogKernel, DiagKernel
@@ -12,6 +14,8 @@ __all__ = ["StaticAbstractKernel", "AbstractKernel",
            "StaticSEMagmaKernel", "SEMagmaKernel",
            "StaticConstantKernel", "ConstantKernel",
            "StaticLinearKernel", "LinearKernel",
+           "StaticPeriodicKernel", "PeriodicKernel",
+           "StaticRationalQuadraticKernel", "RationalQuadraticKernel",
         #    "StaticMaternKernel", "MaternKernel",
            "OperatorKernel", "SumKernel", "ProductKernel",
            "WrapperKernel", "NegKernel", "ExpKernel", "LogKernel", "DiagKernel"]
