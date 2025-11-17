a7e8964b95d535365697333577468575f6156d01
SimLej18 <44818176+SimLej18@users.noreply.github.com>
2025-11-17 11:24:01 +0100

    [feat] first k_means implementation for initital mixture

--- MagmaClustPy/initialisation.py
+++ MagmaClustPy/initialisation.py
@@ -6,4 +6,37 @@ This script will feature functions used to initialise elements of the MagmaClust
 * ...
 """
 
-# TODO
+import jax.numpy as jnp
+from MagmaClustPy.kmeans import k_means
+
+# TODO: init_kernels
+
+# TODO: init_prior_mean
+
+
+def k_means_init(padded_outputs, k, distinct_hp=False):
+	"""
+	Compute the initial assignment of outputs between k clusters using k_means and a naive dimensionality reduction based on task statistics (min, mean, max)
+
+	:param padded_outputs: the outputs from each task, (jnp.array, shape=(T, Max_N))
+	:param k: the number of clusters (int)
+	:param distinct_hp: whether distinct hyperparameters are used (bool, default=False)
+	:return: the initial mixture as a jnp.array of shape (T,)
+	"""
+	# Compute statistics
+	features = jnp.stack([
+		jnp.nanmin(padded_outputs, axis=0),  # Min
+		jnp.nanmean(padded_outputs, axis=0),  # Mean
+		jnp.nanmax(padded_outputs, axis=0)  # Max
+	], axis=-1).squeeze()
+
+	if distinct_hp:
+		features = jnp.concatenate([
+			features,
+			jnp.nanvar(padded_outputs, axis=0, ddof=1).reshape(-1, 1)  # Variance
+		], axis=-1)
+
+	# Run k-means
+	_, labels, _= k_means(features, n_clusters=k, n_init=10, max_iter=100)
+
+	return jnp.array(labels)
