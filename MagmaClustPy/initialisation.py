3dcd6d5d0dcf0a0dab97de47a4a23776be34b0ff
SimLej18 <44818176+SimLej18@users.noreply.github.com>
2025-11-18 15:22:11 +0100

    [feat] initial mixture and post_mean/post_cov for each cluster finalized

--- MagmaClustPy/initialisation.py
+++ MagmaClustPy/initialisation.py
@@ -5,8 +5,11 @@ This script will feature functions used to initialise elements of the MagmaClust
 * initial mixture of mean processes
 * ...
 """
+from functools import partial
 
 import jax.numpy as jnp
+from jax import jit
+
 from MagmaClustPy.kmeans import k_means
 
 # TODO: init_kernels
@@ -14,6 +17,7 @@ from MagmaClustPy.kmeans import k_means
 # TODO: init_prior_mean
 
 
+@partial(jit, static_argnums=(1, 2))
 def k_means_init(padded_outputs, k, distinct_hp=False):
 	"""
 	Compute the initial assignment of outputs between k clusters using k_means and a naive dimensionality reduction based on task statistics (min, mean, max)
@@ -21,22 +25,27 @@ def k_means_init(padded_outputs, k, distinct_hp=False):
 	:param padded_outputs: the outputs from each task, (jnp.array, shape=(T, Max_N))
 	:param k: the number of clusters (int)
 	:param distinct_hp: whether distinct hyperparameters are used (bool, default=False)
-	:return: the initial mixture as a jnp.array of shape (T,)
+	:return: the initial mixture as a one-hot encoded array (jnp.array, shape=(k, T))
 	"""
+	outputs = padded_outputs.squeeze().T
+
 	# Compute statistics
 	features = jnp.stack([
-		jnp.nanmin(padded_outputs, axis=0),  # Min
-		jnp.nanmean(padded_outputs, axis=0),  # Mean
-		jnp.nanmax(padded_outputs, axis=0)  # Max
+		jnp.nanmin(outputs, axis=0),  # Min
+		jnp.nanmean(outputs, axis=0),  # Mean
+		jnp.nanmax(outputs, axis=0)  # Max
 	], axis=-1).squeeze()
 
 	if distinct_hp:
 		features = jnp.concatenate([
 			features,
-			jnp.nanvar(padded_outputs, axis=0, ddof=1).reshape(-1, 1)  # Variance
+			jnp.nanvar(outputs, axis=0, ddof=1).reshape(-1, 1)  # Variance
 		], axis=-1)
 
 	# Run k-means
 	_, labels, _= k_means(features, n_clusters=k, n_init=10, max_iter=100)
 
-	return jnp.array(labels)
+	# One-hot encode labels
+	labels = jnp.eye(k)[labels]
+
+	return labels.T
